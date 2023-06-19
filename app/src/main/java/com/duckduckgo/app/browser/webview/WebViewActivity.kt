acc0ddec89fa5f43e63a75b7490fa575509a7221
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-19 16:26:13 +0530

    Update WebViewActivity.kt

--- app/src/main/java/com/duckduckgo/app/browser/webview/WebViewActivity.kt
+++ app/src/main/java/com/duckduckgo/app/browser/webview/WebViewActivity.kt
@@ -59,7 +59,8 @@ class WebViewActivity : DuckDuckGoActivity() {
             it.webViewClient = webViewClient
 
             it.settings.apply {
-                userAgentString = userAgentProvider.userAgent()
+                userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
+//                userAgentString = userAgentProvider.userAgent()
                 javaScriptEnabled = true
                 domStorageEnabled = true
                 loadWithOverviewMode = true
